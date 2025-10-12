b08095113dd8da2523accf8954b3f5516ee17742
zubairansari-codes <zubairansari.codes@gmail.com>
2025-10-12 21:07:53 +0530

    Create UserRepository.java

---
+++ backend/src/main/java/com/zubair/travel/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.zubair.travel.repository;
+
+import com.zubair.travel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
