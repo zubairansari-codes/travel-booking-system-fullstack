65657f7f1f126e16c56727f0823cf4309c6b8310
zubairansari-codes <zubairansari.codes@gmail.com>
2025-10-12 20:46:52 +0530

    Init: Spring Boot backend project with entrypoint

---
+++ backend/src/main/java/com/zubair/travel/TravelBookingApplication.java
@@ -0,0 +1,12 @@
+package com.zubair.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBookingApplication.class, args);
+    }
+}
