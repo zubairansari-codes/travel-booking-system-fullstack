9685497573fde58ec035fcac5ea55c57ab02bba9
zubairansari-codes <zubairansari.codes@gmail.com>
2025-10-12 21:32:25 +0530

    Add UserRepository.java

---
+++ docs/backend/src/main/java/com/zubair/travel/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.zubair.travel.repository;
+
+import com.zubair.travel.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+}
