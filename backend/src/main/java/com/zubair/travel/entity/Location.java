49a13fc7802535c2a3e4473cb8c3adc143be17a5
zubairansari-codes <zubairansari.codes@gmail.com>
2025-10-12 21:00:47 +0530

    Add Location entity

---
+++ backend/src/main/java/com/zubair/travel/entity/Location.java
@@ -0,0 +1,30 @@
+package com.zubair.travel.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "locations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    
+    @Column(nullable = false)
+    private String state;
+    
+    @Column(nullable = false)
+    private String country;
+}
